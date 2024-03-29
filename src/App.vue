866c8866aa8834d87c8fc4379d71fc2b97f4f9a8
SasciaCode <165293894+SasciaCode@users.noreply.github.com>
2024-03-29 18:48:55 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Signika Negative", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,238 @@
+<template>
+  <section id="home">  </section>
+    <div class="col-lg-12 fixed-top">
+      <div class="d-flex justify-content-center py-4">
+        <nav class="navbar navbar-expand-lg bg-light d-none d-lg-block rounded-pill shadow px-3">
+          <div class="container-fluid">
+            <a class="navbar-brand title me-5 fs-4" href="#">Christine</a>
+            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link header fs-5" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link header fs-5" href="#skills">Skills</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link header fs-5" href="#projects">Projects</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link header fs-5" href="#contact">Contacts</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link header fs-5" href="#about">About</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+
+  <p class="text-center title main px-md-4">Christine Mae Angco</p>
+  <h1 class="content header">Let's get to know!</h1>
+
+  <div class="container-fluid px-5 py-4">
+    <div class="row">
+      <div class="card m-top border-0 shadow">
+        <div class="card-body">
+          <div class="row justify-content-between px-4">
+            <div class="col-lg-5">
+              <img src="profile.png" alt="" class="card-img img-fluid">
+            </div>
+            <div class="col-lg-5 p-lg-">
+                <div class="mb-3 text-start">
+                  <label for="name" class="form-label header fs-5 mt-3">Name</label>
+                  <input type="text" class="form-control fs-5 rounded-pill bg-light border-0" value="Christine Mae R. Angco" disabled>
+                </div>
+                <div class="row">
+                  <div class="mb-3 col-lg-6 col-md-12 text-start">
+                    <label for="name" class="form-label header fs-5">Age</label>
+                    <input type="text" class="form-control fs-5 rounded-pill bg-light border-0" value="21 years old" disabled>
+                  </div>
+                  <div class="mb-3 col-lg-6 col-md-12 text-start">
+                    <label for="name" class="form-label header fs-5">Birthdate</label>
+                    <input type="text" class="form-control fs-5 rounded-pill bg-light border-0" value="May 5, 2002" disabled>
+                  </div>
+                </div>
+                <div class="mb-3 text-start">
+                  <label for="name" class="form-label header fs-5 mt-3">Address</label>
+                  <input type="text" class="form-control fs-5 rounded-pill bg-light border-0" value="Catarman, Cordova, Cebu" disabled>
+                </div>
+                <div class="mb-3 text-start">
+                  <label for="name" class="form-label header fs-5 mt-3">Hobbies</label>
+                  <input type="text" class="form-control fs-5 rounded-pill bg-light border-0" value="Reading stories, listening to music, watching videos" disabled>
+                </div>
+                <div class="mb-3 text-start">
+                  <label for="name" class="form-label header fs-5 mt-3">Likes</label>
+                  <input type="text" class="form-control fs-5 rounded-pill bg-light border-0" value="Humba, dogs, sea, pop music, hanging out with friends, family" disabled>
+                </div>
+                <div class="mb-3 text-start">
+                  <label for="name" class="form-label header fs-5 mt-3">Dislikes</label>
+                  <input type="text" class="form-control fs-5 rounded-pill bg-light border-0" value="Noisy places, crowded places, fish, dancing" disabled>
+                </div>
+            </div>
+          </div>  
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section id="skills"></section>
+  <h1 class="display-1 fw-bold py-4">Skills</h1>
+  <div class="container-fluid px-5 py-4">
+    <div class="row">
+      <div class="col-lg-6 py-4">
+        <div class="card border-0 shadow mb-3">
+          <div class="row g-0 d-flex align-items-center">
+            <div class="col-md-4">
+              <img src="adapt.png" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h1 class="card-title title">Adaptability</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6 py-4">
+        <div class="card border-0 shadow mb-3">
+          <div class="row g-0 d-flex align-items-center">
+            <div class="col-md-4">
+              <img src="s-improve.png" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h1 class="card-title title">Self-improvement</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section id="projects"></section>
+  <h1 class="display-1 fw-bold py-4">Projects</h1>
+  <div class="container-fluid col-lg-8 col-md-12 px-5 py-4">
+    <div class="row">
+      <div class="col-lg-12 py-4">
+        <div class="card border-0 shadow mb-3">
+          <div class="row g-0 d-flex align-items-center">
+            <div class="col-md-12">
+              <img src="https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/434332972_376035948736407_2665399726240780082_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGneIXec46J3iJK8FkAAN19Pk8j1HKFVwE-TyPUcoVXAWVDNiHgH0bbcN7A21hbkL97WqkBDObkv6x8uNTa1xbz&_nc_ohc=XnwEgXm4bRoAX8GTicN&_nc_ht=scontent-mnl1-1.xx&oh=00_AfAyS3a1Zjpp3eY1Sty1BaUK7yi-qK3qNBoAMsjDLlP8Ew&oe=660BCDCF" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-12 py-4">
+        <div class="card border-0 shadow mb-3">
+          <div class="row g-0 d-flex align-items-center">
+            <div class="col-md-12">
+              <img src="https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/434227129_376038618736140_5192826444597797892_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHcJMYFHRDixEB9TOG9leMGzKIuqOAaF-3Moi6o4BoX7VneZfOKW1UWfNZgoKSg1rcCxleWplM0Q5gqz0GX4uPS&_nc_ohc=oU-PhenNbsAAX82YYBY&_nc_ht=scontent-mnl1-1.xx&oh=00_AfD7KDn6cwbSlkphY5MMsxYW9t9CEbMqjn1UXOJAyL5_kA&oe=660B4913" class="img-fluid rounded-start p-4" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section id="contact"></section>
+  <h1 class="display-1 fw-bold py-4">Contacts</h1>
+    <div class="container-fluid px-5 py-4">
+      <div class="row justify-content-between">
+        <div class="col-lg-3 col-md-6">
+          <a href="https://www.facebook.com/christinemaeangco" class="text-decoration-none">
+            <img src="fb.png" alt="" class="img-fluid py-4" width="50">
+            <h4 class="bg-white d-lg-block d-none rounded-pill shadow text-dark">Christine Mae Angco</h4>
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <a href="https://www.instagram.com/kr.istine3319?igsh=ZTRqOXJyeGFhMHcy" class="text-decoration-none">
+            <img src="ig.png" alt="" class="img-fluid py-4" width="50">
+            <h4 class="bg-white d-lg-block d-none rounded-pill shadow text-dark">kr.istine3319</h4>
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <a href="gmail.com" class="text-decoration-none">
+            <img src="gmail.png" alt="" class="img-fluid py-4" width="50">
+            <h4 class="bg-white d-lg-block d-none rounded-pill shadow text-dark">christinemaeangco22@gmail.com</h4>
+          </a>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <a href="" class="text-decoration-none">
+            <img src="tel.png" alt="" class="img-fluid py-4" width="50">
+            <h4 class="bg-white d-lg-block d-none rounded-pill shadow text-dark">+639099481052</h4>
+          </a>
+        </div>
+      </div>
+    </div>
+
+  <section id="about"></section>
+  <h1 class="display-1 fw-bold py-4">About</h1>
+    <div class="container-fluid px-5 py-4">
+      <div class="bg-white shadow border-0 p-4">
+        <h4>
+          Welcome to my portfolio, where creativity meets functionality! As a passionate developer, I strive to craft engaging experiences that captivate audiences and leave a lasting impression. Through a blend of innovation, skill, and attention to detail, I bring ideas to life in ways that inspire and delight. Explore my work and discover the essence of my dedication to excellence. Let's create something extraordinary together!
+        </h4>
+      </div>
+    </div>
+</template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+
+  <style scoped>
+  .title {
+    font-family: "Acme", sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  .main {
+    margin-top: 270px;
+    font-size: 180px;
+  }
+
+  .m-top {
+    margin-top: 270px;
+  }
+
+  .content {
+    margin-top: -70px;
+  }
+  
+  h3 {
+    margin: 40px 0 0;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  @media (max-width: 767px) {
+    .main {
+      margin-top: 100px;
+      font-size: 120px;
+    }
+
+    .content {
+      margin-top: -30px;
+    }
+  }
+  </style>
+  
